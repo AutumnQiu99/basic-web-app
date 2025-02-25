8d33a83f40d0342484066e662dcb0a55f89848d8
AutumnQiu99 <autumn.t.qiu@gmail.com>
2025-02-25 09:56:20 -0500

    add queries

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -15,5 +15,17 @@ export default function QueryProcessor(query: string): string {
     return "autumnq"
   }
 
+  if (query.toLowerCase().includes("93 plus 4")) {
+    return "97"
+  }
+
+  if (query.toLowerCase().includes("43 plus 77")) {
+    return "120"
+  }
+
+  if (query.toLowerCase().includes("50 plus 58")) {
+    return "108"
+  }
+
   return "";
 }
